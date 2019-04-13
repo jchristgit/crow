898d9de2959e72a0238da2ba2138ae2d7e49a3c8
Johannes Christ <jc@jchri.st>
2019-04-13 15:00:14 +0200

    Expand Plugin behaviour documentation.

--- lib/crow/plugin.ex
+++ lib/crow/plugin.ex
@@ -1,6 +1,38 @@
 defmodule Crow.Plugin do
   @moduledoc """
   The behaviour all configured plugins should implement.
+
+  ## Overview
+
+  Writing [Munin plugins](http://guide.munin-monitoring.org/en/latest/plugin/index.html)
+  is rather simple. A trivial plugin with the default
+  Munin node setup is a script with two main behaviours:
+
+  - When invoked with the command line argument `config`, print out configuration
+    of the plugin, such as graph setup (title, category, labels), field information
+    and more. A full description of all options can be found at the [Plugin reference](
+    http://guide.munin-monitoring.org/en/latest/reference/plugin.html) documentation.
+
+  - When not invoked with any command line argument, print out the values for fields
+    declared in the `config` command.
+
+  This is the foundation from which plugins are developed. Since our node runs in
+  the BEAM, we've taken a different approach here. Instead of being executable shell
+  scripts, crow plugins are modules which provide two main functions:
+
+  - `c:config/0`, which returns the configuration of the plugin, corresponding to
+    the first invocation form described above.
+
+  - `c:values/0`, which returns the values of the plugin, corresponding to the
+    second invocation form described above.
+
+  Instead of printing to standard output, these return a list of charlists which
+  is then sent to the peer via TCP.
+
+  ## Community plugins
+
+  Plugins for the crow node can be found in the
+  [`crow_plugins`](https://github.com/jchristgit/crow_plugins) repository.
   """
 
   @doc """
@@ -10,7 +42,7 @@ defmodule Crow.Plugin do
   Adding newlines to each line is done by the worker.
 
   For reference, see [Munin plugin config command](http://guide.munin-monitoring.org/en/latest/develop/plugins/howto-write-plugins.html#munin-plugin-config-command)
-  from the official guide.
+  from the official documentation.
 
   ## Example
 
@@ -42,10 +74,13 @@ defmodule Crow.Plugin do
   @callback values() :: [charlist()]
 
   @doc """
-  Return the name of the plugin displayed to connections.
-  By default, a name is generated using `Crow.Helpers.plugin_name/1`.
+  Return the name of the plugin displayed to peers.
+
+  If this callback is not defined, a name is generated based on the
+  module name of the plugin. See `Crow.Helpers.plugin_name/1` for more
+  information.
   """
-  @callback name() :: String.t()
+  @callback name() :: charlist()
 
   @optional_callbacks [name: 0]
 end
